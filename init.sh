6170fb071915d46dc80ee8dab86a29d904f404c6
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-05-22 01:16:20 -0400

    fix

--- init.sh
+++ init.sh
@@ -9,4 +9,4 @@ mkdir celestia-pwd
 sudo chown 10001:10001 ./celestia-pwd
 
 # create keys
-nohup docker run -e NODE_TYPE=light -e P2P_NETWORK=blockspacerace -v ./celestia-pwd:/home/celestia ghcr.io/celestiaorg/celestia-node:v0.9.5 celestia light init --p2p.network blockspacerace
+docker run -e NODE_TYPE=light -e P2P_NETWORK=blockspacerace -v ./celestia-pwd:/home/celestia ghcr.io/celestiaorg/celestia-node:v0.9.5 celestia light init --p2p.network blockspacerace
